6c02e8152f07ca0d21ad3c0757ae35643aeec7a8
mathslimin <mathslimin@gmail.com>
2022-05-18 14:54:21 +0800

    Update config.el

--- config.el
+++ config.el
@@ -74,7 +74,49 @@ confirm-kill-emacs nil
   '(default ((t (:background "#000000"))))
   '(hl-line ((t (:background "#000000"))))
  )
+
+;;golang
+
+(setq lsp-gopls-staticcheck t)
+(setq lsp-eldoc-render-all t)
+(setq lsp-gopls-complete-unimported t)
+
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :hook (go-mode . lsp-deferred))
+
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+;; Optional - provides fancier overlays.
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+
+;; Company mode is a standard completion package that works well with lsp-mode.
+(use-package company
+  :ensure t
+  :config
+  ;; Optionally enable completion-as-you-type behavior.
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1))
+
+;; Optional - provides snippet support.
+(use-package yasnippet
+  :ensure t
+  :commands yas-minor-mode
+  :hook (go-mode . yas-minor-mode))
+
+;;rust
+(setq rustic-lsp-server 'rust-analyzer)
+
 ;;cmake-mode disable format
 (setq-hook! 'cmake-mode-hook +format-with :none)
 (setq-hook! 'c-mode-hook +format-with :none)
 (setq-hook! 'cpp-mode-hook +format-with :none)
+
